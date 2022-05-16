f4030b84770e5ea43f11149aed54b9faa85ec994
blogghar <sks.deepak2@gmail.com>
2022-05-16 21:55:43 +0530

    created login registration

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const connectDB = require("./db");
+
+//Connecting the Database
+connectDB();
+
+
+const app = express()
+const PORT = 5000
+
+app.use(express.json())
+app.use("/api/auth", require("./auth/route"))
+
+
+
+const server = app.listen(PORT, () =>
+  console.log(`Server Connected to port ${PORT}`)
+)
+// Handling Error
+process.on("unhandledRejection", err => {
+  console.log(`An error occurred: ${err.message}`)
+  server.close(() => process.exit(1))
+})
